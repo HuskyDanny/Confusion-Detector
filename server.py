d0c522c916ef6bc44e3726c5459775894e2cea29
Junchen Pan <40053881+HuskyDanny@users.noreply.github.com>
2019-02-24 01:54:45 -0800

    Add files via upload

--- server.py
+++ server.py
@@ -10,30 +10,38 @@ app = Flask(__name__)
 app.secret_key = 'mysecret'
 socket_io = SocketIO(app)
 
-# _mode = 'stop'
+_mode = 'stop'
 
 @app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/charts', methods=['POST'])
 def draw():
-    print("render conplete")
-    return render_template('index.html', x_window=100)
+    if request.method == 'POST':
+        return render_template('charts.html', x_window=100)
 
 # Receiving Messages
 @socket_io.on('my_event')
 def drawer(data):
-    print('input data: ' + str(data))
-    socket_io.emit('update', data)
+    global _mode
+    if _mode == 'stop':
+        pass
+    else:
+        print('input data: ' + str(data))
+        socket_io.emit('update', data)
 
 @socket_io.on('my_ping')
 def ping_pong():
     emit('my_pong')
 
-# @socket_io.on('change mode')
-# def changer(data):
-#     global _mode
-#     if data['mode'] == 'start':
-#         _mode = 'start'
-#     else:
-#         _mode = 'stop'
+@socket_io.on('change mode')
+def changer(data):
+    global _mode
+    if data['mode'] == 'start':
+        _mode = 'start'
+    else:
+        _mode = 'stop'
 
 if __name__ == '__main__':
     socket_io.run(app, debug=True, host='localhost', port=8000)
